054eed131aa7db453d21b82d3537bd8e0f939e06
ShahnazKhanum16 <85401790+ShahnazKhanum16@users.noreply.github.com>
2023-09-06 21:56:15 +0530

    newcommit

--- InstApp/urls.py
+++ InstApp/urls.py
@@ -3,5 +3,13 @@ from .import views
 
 urlpatterns = [
     path('', views.home, name ='home'),
+    path('Search/', views.search, name ='search'),
+    path('Explore/', views.explore, name ='explore'),
+    path('Reels/', views.reels, name ='reels'),
+    path('Messages/', views.messages, name ='messages'),
+    path('Notification/', views.notification, name ='notification'),
+    path('Create/', views.create, name ='create'),
+    path('Profile/', views.profile, name ='profile'),
+    path('More/', views.more, name ='more'),
     
 ]
--- InstApp/views.py
+++ InstApp/views.py
@@ -2,4 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home (request):
-    return render(request,'main.html')
+    return render(request,'home.html')
+
+def search (request):
+    return render(request,'search.html')
+
+def explore (request):
+    return render(request,'explore.html')
+
+def reels (request):
+    return render(request,'reels.html')
+
+def messages (request):
+    return render(request,'messages.html')
+
+def notification (request):
+    return render(request,'notification.html')
+
+def create (request):
+    return render(request,'create.html')
+
+def profile (request):
+    return render(request,'profile.html')
+
+def more (request):
+    return render(request,'more.html')
